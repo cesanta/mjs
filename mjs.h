46f5acffec93362b955638e6bd8fad1645b5e74e
Dmitry Frank <mail@dmitryfrank.com>
2017-01-11 00:47:07 +0000

    Implement arrays
    PUBLISHED_FROM=a19faaa1521deb5d422ac7db5c96f9d7404b0ea1

--- mjs.h
+++ mjs.h
@@ -227,6 +227,56 @@ const char *mjs_strerror(struct mjs *mjs, mjs_err_t err);
  * if you want to contribute them back.
  */
 #ifdef MG_MODULE_LINES
+#line 1 "mjs/array_public.h"
+#endif
+/*
+ * Copyright (c) 2014 Cesanta Software Limited
+ * All rights reserved
+ */
+
+/*
+ * === Arrays
+ */
+
+#ifndef MJS_ARRAY_PUBLIC_H_
+#define MJS_ARRAY_PUBLIC_H_
+
+/* Amalgamated: #include "mjs/core_public.h" */
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /* __cplusplus */
+
+/* Make an empty array object */
+mjs_val_t mjs_mk_array(struct mjs *mjs);
+
+/* Returns length on an array. If `arr` is not an array, 0 is returned. */
+unsigned long mjs_array_length(struct mjs *mjs, mjs_val_t arr);
+
+/* Insert value `v` in array `arr` at the end of the array. */
+mjs_err_t mjs_array_push(struct mjs *mjs, mjs_val_t arr, mjs_val_t v);
+
+/*
+ * Return array member at index `index`. If `index` is out of bounds, undefined
+ * is returned.
+ */
+mjs_val_t mjs_array_get(struct mjs *, mjs_val_t arr, unsigned long index);
+
+/* Insert value `v` into `arr` at index `index`. */
+mjs_err_t mjs_array_set(struct mjs *mjs, mjs_val_t arr, unsigned long index,
+                        mjs_val_t v);
+
+#if 0
+/* Delete value in array `arr` at index `index`, if it exists. */
+void mjs_array_del(struct mjs *mjs, mjs_val_t arr, unsigned long index);
+#endif
+
+#if defined(__cplusplus)
+}
+#endif /* __cplusplus */
+
+#endif /* MJS_ARRAY_PUBLIC_H_ */
+#ifdef MG_MODULE_LINES
 #line 1 "mjs/core_public.h"
 #endif
 /*
