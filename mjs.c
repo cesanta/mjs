1e19c481a61b8312aac50a9d19135526c45780bf
Dmitry Frank <mail@dmitryfrank.com>
2018-01-29 23:49:31 +0200

    Include stdbool.h for NRF52
    Fixes https://github.com/cesanta/mjs/issues/82
    Integrates https://github.com/cesanta/mjs/pull/85
    
    PUBLISHED_FROM=2f5f50c25ec1c92fdfb783cd95cb57f0cb20642b

--- mjs.c
+++ mjs.c
@@ -1162,6 +1162,7 @@ int gettimeofday(struct timeval *tp, void *tzp);
 #include <ctype.h>
 #include <errno.h>
 #include <inttypes.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
 #include <time.h>
